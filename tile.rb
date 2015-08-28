5265c0971d6dd4e9b6129b522f0356bc22aae082
Patrick Sandquist <patricksandquist@gmail.com>
2015-08-28 11:06:46 -0700

    Add reveal and neighbor_bomb_count

--- tile.rb
+++ tile.rb
@@ -1,5 +1,9 @@
 class Tile
+  NEIGHBORS = [[0, 1], [1, 1], [1, 0], [1, -1],
+               [0, -1], [-1, -1], [-1, 0], [-1, 1]]
+
   attr_accessor :status
+  attr_reader :bomb
 
   def initialize(board, pos, bomb = false)
     @board = board
@@ -19,4 +23,21 @@ class Tile
   def revealed?
     @status == :revealed
   end
+
+  def reveal
+    return :bomb if @bomb
+    self.status = :revealed
+
+    neighbor_bomb_count
+  end
+
+  def neighbor_bomb_count
+
+    NEIGHBORS.inject(0) do |count, dis|
+      x, y = dis[0] + pos[0], dis[1] + pos[1]
+      count + 1 if board[[x, y]].bomb
+    end
+
+  end
+
 end
